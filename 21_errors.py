a8272af718832c1a9e6feebc27399fa1ae8c5277
rockband_666 <rockband_666@hotmail.com>
2023-11-23 10:19:02 -0400

    escepciones como hacer nuestros errores para que se detenga hasta ahi

---
+++ 21_errors.py
@@ -0,0 +1,15 @@
+# print(0/0)
+# print(result)
+
+print("hola")
+
+suma = lambda x,y : x + y
+assert suma(2,2) == 4
+
+print("hola 2")
+
+age = 10
+if age < 18:
+    raise Exception("Nose permite menores de edad")
+
+print("hola 2")
